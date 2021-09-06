30711eee03efd7c3d7784e7a671bf99ee5ee2465
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-09-05 20:19:06 -0700

    connected to the mysql database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
 PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MYSQL username,
+        user: 'root',
+        // Your MYSQL password
+        password: '1hyh9jaf',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
